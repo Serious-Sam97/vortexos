3b6d76284ef5373ae7993ee31d758758dec92978
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2025-01-22 12:42:48 -0300

    SpringBoot setup

---
+++ src/main/java/com/serioussam/gamecache/gamecache/GamecacheApplication.java
@@ -0,0 +1,13 @@
+package com.serioussam.gamecache.gamecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serioussam/gamecache/gamecache/GamecacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serioussam.gamecache.gamecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
