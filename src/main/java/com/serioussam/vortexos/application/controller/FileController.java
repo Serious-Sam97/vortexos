be487f168189f53b1ea2e3e9a41bc2c5adffa7b0
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2025-03-12 11:45:09 -0300

    On FileController, fixed index route

--- src/main/java/com/serioussam/vortexos/application/controller/FileController.java
+++ src/main/java/com/serioussam/vortexos/application/controller/FileController.java
@@ -3,6 +3,7 @@ package com.serioussam.vortexos.application.controller;
 import com.serioussam.vortexos.domain.file.File;
 import com.serioussam.vortexos.domain.file.FileRepository;
 import com.serioussam.vortexos.infrastructure.repository.JpaFileRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class FileController {
         this.fileRepository = fileRepository;
     }
 
+    @GetMapping
     public List<File> index()
     {
         return this.fileRepository.findAll();
